129c8fd846c9c40264aa75d75c55a557ed71b28e
Gaspar <e972136@gmail.com>
2023-11-08 19:26:41 -0600

    Me faltaba el tryCatch

--- Program.cs
+++ Program.cs
@@ -12,12 +12,24 @@ Clase clase = new Clase(
 clase.mostrarClase();
 clase.mostrarAlumno();
 
-Console.Write("Ingrese nota 1: ");
-int nota1 =  Convert.ToInt32(Console.ReadLine());
-Console.Write("Ingrese nota 2: ");
-int nota2 =  Convert.ToInt32(Console.ReadLine());
-Console.Write("Ingrese nota 3: ");
-int nota3 =  Convert.ToInt32(Console.ReadLine());
+int nota1;
+int nota2;
+int nota3;
+
+try{
+    Console.Write("Ingrese nota 1: ");
+    nota1 =  Convert.ToInt32(Console.ReadLine());
+    Console.Write("Ingrese nota 2: ");
+    nota2 =  Convert.ToInt32(Console.ReadLine());
+    Console.Write("Ingrese nota 3: ");
+    nota3 =  Convert.ToInt32(Console.ReadLine());
+}catch(Exception e){
+    Console.WriteLine("Error en ingreso:\n"+e);    
+    nota1 = 0;
+    nota2 = 0;
+    nota3 = 0;
+}
+
 
 Promedio promedio = new Promedio(nota1,nota2,nota3);
 Console.WriteLine(promedio.mostrarPromedio(nota1,nota2,nota3));
